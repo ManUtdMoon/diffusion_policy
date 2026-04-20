3d8933a0c75a24b7d75db632300e02682d28e07b
manutdmoon <yudongjie.moon@foxmail.com>
2026-04-20 15:13:49 +0800

    fix: try transform both poses to real tcp

--- eval_offline.sh
+++ eval_offline.sh
@@ -4,8 +4,8 @@ export MKL_NUM_THREADS=1
 
 
 python eval_real.py \
-    -c data/outputs/2026.04.16/00.39.57_train_flow_match_vib_unet_image_ddp_wallet/checkpoints/latest.ckpt \
-    -o data/eval/wallet/test_1st_aug \
+    -c data/outputs/2026.04.18/18.45.03_train_flow_match_vib_unet_image_ddp_wallet/checkpoints/latest.ckpt \
+    -o data/eval/wallet/test_2nd_tcp-Ta24 \
     -d cuda:0 \
-    -t 16 \
-    -s 5
+    -t 24 \
+    -s 10
