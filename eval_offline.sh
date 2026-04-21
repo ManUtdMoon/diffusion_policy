6c8a6ebac747a9cb4b71181efcb1ae9690b57b52
manutdmoon <yudongjie.moon@foxmail.com>
2026-04-21 11:07:05 +0800

    fix: start from both tcp

--- eval_offline.sh
+++ eval_offline.sh
@@ -4,8 +4,8 @@ export MKL_NUM_THREADS=1
 
 
 python eval_real.py \
-    -c data/outputs/2026.04.18/18.45.03_train_flow_match_vib_unet_image_ddp_wallet/checkpoints/latest.ckpt \
-    -o data/eval/wallet/test_2nd_tcp-Ta24 \
+    -c data/outputs/2026.04.20/15.30.42_train_flow_match_vib_unet_image_ddp_wallet/checkpoints/latest.ckpt \
+    -o data/eval/wallet/test_3rd-both_tcp-Ta24 \
     -d cuda:0 \
     -t 24 \
     -s 10
